674701847e47417480f2f89293a5f848d1d663db
eslamber <eslamber@student.42lyon.fr>
2024-08-30 17:10:46 +0200

    ca fonctionne, j'ai réussis a connecter da=jango avec la base de donnée et a lancer le site

--- Docker/django/tools/launch.sh
+++ Docker/django/tools/launch.sh
@@ -6,32 +6,35 @@
 #    By: eslamber <eslamber@student.42lyon.fr>      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/26 16:19:02 by eslamber          #+#    #+#              #
-#    Updated: 2024/08/27 17:20:17 by eslamber         ###   ########.fr        #
+#    Updated: 2024/08/30 16:54:30 by eslamber         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#!/bin/sh
+#!/bin/bash
 
 # Lancement de la base de donnée sans quoi rien ne fonctionnerait
 # Pas sur que ce soit toujours utile
 # su - postgres -c "pg_ctl -D /var/lib/postgresql/data -l logfile start"
-su - "$USER" -c "/usr/lib/postgresql/13/bin/pg_ctl -D "$DATA_DIR" -l /var/lib/postgresql/logfile start"
+# su - "$USER" -c "/usr/lib/postgresql/13/bin/initdb --locale=$LANG -D $DATA_DIR"
+# su - "$USER" -c "/usr/lib/postgresql/13/bin/pg_ctl -D "$DATA_DIR" -l /var/lib/postgresql/logfile start"
 
-# python3 manage.py dbshell
+# python3 $VOLUME/manage.py dbshell
+
+# sleep 10
 
 # Créer des migrations
-python3 /run/app/manage.py makemigrations
+python3 $VOLUME/manage.py makemigrations
 
 # Appliquer les migrations
-python3 /run/app/manage.py migrate
+python3 $VOLUME/manage.py migrate
 
 # Collecter les fichiers statiques
-python3 /run/app/manage.py collectstatic --noinput
+# python3 $VOLUME/manage.py collectstatic --noinput
 
 # Créer un superutilisateur automatiquement
 if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ] && [ "$DJANGO_SUPERUSER_EMAIL" ]; then
-    python3 /run/app/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+    python3 $VOLUME/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 fi
 
 # Lancer le serveur Django
-python3 /run/app/manage.py runserver 0.0.0.0:8000
+python3 $VOLUME/manage.py runserver 0.0.0.0:8000
