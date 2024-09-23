83fb84e6f7716dbd3fa1c9dfa7d5d5f127042a55
eslamber <eslamber@student.42lyon.fr>
2024-09-23 13:46:25 +0200

    tout est bon pour les postgresql maintenant (création du roleeste et de la base de donnée este dans le script init.sql dans chaque service contenant le conteneur postgresql

---
+++ Docker/API/postgresql/tools/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE este;
+
+CREATE OR REPLACE FUNCTION create_role_if_not_exists() 
+RETURNS VOID AS $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'este') THEN
+        CREATE ROLE este WITH LOGIN PASSWORD '123soleil';
+        ALTER ROLE este CREATEDB; -- Permet de créer des bases de données
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT create_role_if_not_exists();
