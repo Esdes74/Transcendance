702f1b2c48f85afffe47a190303f45a40d3f02e9
tim <tdutel@LAPTOP>
2024-09-25 14:59:55 +0200

    add view and update settings.py of project. todo:fix freezepong

--- Pong_project/Pong_project/settings.py
+++ Pong_project/Pong_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,19 +55,19 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Pong_project.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [os.path.join(BASE_DIR, 'pong/templates')],  # Spécifie le chemin des templates
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 ASGI_APPLICATION = 'Pong_project.asgi.application'
@@ -117,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'pong/static')]  # Spécifie le chemin des fichiers statiques
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Pong_project/pong/views.py
+++ Pong_project/pong/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.shortcuts import render
+
 def pong_view(request):
-	return render(request, 'pong_app/pong.html')
+	return render(request, 'pong.html')  # Assure-toi que 'pong.html' est dans ton répertoire de templates
