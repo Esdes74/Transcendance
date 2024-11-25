e2be08c7b86839ddc59c3fbdc1bb773b3e7e1d7a
Loic Mohin <lmohin@z3r2p1.42lyon.fr>
2024-11-25 17:29:19 +0100

    update dropdown menu appearance for flags, base language set to french

--- Frontend/S_Front/js/aff_index.js
+++ Frontend/S_Front/js/aff_index.js
@@ -5,16 +5,16 @@ function affIndex()
        	<div class="container text-center my-5">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
-				<h1 class="mb-4 fw-bold" data-translate="true">PONG MAJESTUEUX</h1>
+				<h1 class="mb-4 fw-bold" data-translate="true">Pong Majestueux</h1>
 
 				<div class="canvas-container">
 					<!-- <div id="countdown" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 48px; color: white;"></div> -->
 					<canvas id="pongCanvas" class="w-100" height="400"></canvas>
 					<div id="replayBlock">
-						<h2 class="fw-bold" data-translate="true">Login to play</h2>
+						<h2 class="fw-bold" data-translate="true">Pour jouer, se connecter</h2>
 						<div class="button-group">
-						<button id="YES" class="btn btn-outline-light m-2 fw-bold" data-translate="true">Login</button>
-						<button id="BTH" class="btn btn-outline-light m-2 fw-bold" data-translate="true">Register</button>
+						<button id="YES" class="btn btn-outline-light m-2 fw-bold" data-translate="true">Se connecter</button>
+						<button id="BTH" class="btn btn-outline-light m-2 fw-bold" data-translate="true">Créer un compte</button>
 						</div>
 					</div>
 				</div>
