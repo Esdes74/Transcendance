75b5f9f0f5287659572b04a47f4c540640ef6f2a
AlphomArt <menelik.wubneh@gmail.com>
2024-11-25 16:29:00 +0100

    ok ?

--- Backend/S_Tournament/Tournament/Tournament/asgi.py
+++ Backend/S_Tournament/Tournament/Tournament/asgi.py
@@ -14,3 +14,14 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tournament.settings')
 
 application = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from calcul.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),  # Les requêtes HTTP passent par urls.py
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
