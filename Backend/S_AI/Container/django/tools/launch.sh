fbd0e112b808dc2a4d778139360275a3e8cf67ad
Loic Mohin <loic.mohin@gmail.com>
2024-12-14 21:10:01 +0100

    first tries with dockerAI

--- Backend/S_AI/Container/django/tools/launch.sh
+++ Backend/S_AI/Container/django/tools/launch.sh
@@ -6,19 +6,10 @@
 #    By: eslamber <eslambert@student.42lyon.fr>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/26 16:19:02 by eslamber          #+#    #+#              #
-#    Updated: 2024/12/12 21:53:26 by lmohin           ###   ########.fr        #
+#    Updated: 2024/12/12 22:24:20 by lmohin           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-cd $VOLUME
 
-# Créer des migrations
-python3 manage.py makemigrations
-
-# Appliquer les migrations
-python3 manage.py migrate
-
-# Lancer le serveur Django
-uvicorn Pong.asgi:application --host 0.0.0.0 --port 8005
