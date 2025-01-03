487df45a31e299bbf1fc830405a34c38cce6c4d4
Loic Mohin <loic.mohin@gmail.com>
2025-01-04 00:24:45 +0100

    fixed ai front, almost done

--- Frontend/S_Front/js/aff_ai.js
+++ Frontend/S_Front/js/aff_ai.js
@@ -10,6 +10,55 @@ function other()
 	initPong(AICanvas)
 }
 
+function createReplayBlock()
+{
+	myDiv = createElement(div)
+	
+}
+
+function affReplayBlock()
+{
+	menu = document.querySelector(".replayBlock")
+	menu.innerHTML = `<div class="button-group">
+                                                <h2 id="winMessage"></h2>
+                                                <div class="button-group">
+                                                <button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Replay</Button>
+                                                <button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Change Difficulty</button>
+   			</div>`
+	tradDiv(menu)
+	replayButtons = document.querySelectorAll('main button')
+	replayButtons.forEach( replayButton => {
+		replayButton.addEventListener("click", () => {
+			scorePlayer1 = document.getElementById("scorePlayer1")
+			scorePlayer2 = document.getElementById("scorePlayer2")
+			scorePlayer1.innerText = "0"
+			scorePlayer2.innerText = "0"
+			menu.style.display = "none"
+			addScript("/js/pong.js", other)
+		})
+	})
+}
+
+function manageButtons()
+{
+	buttons = document.querySelectorAll('main button')
+	buttons.forEach( button => {
+	button.addEventListener("click", () => {
+		canvas = document.getElementById("menuCanvas")
+		parents = canvas.parentElement
+		canvas.remove()
+		let aiCanvas = document.createElement("canvas")
+		aiCanvas.id = "AICanvas"
+		aiCanvas.class = "w-100"
+		aiCanvas.height = 400
+		parents.append(aiCanvas)
+		affReplayBlock()
+		document.querySelector(".replayBlock").style.display = "none"
+		addScript("/js/pong.js", other)
+		})
+	})
+}
+
 function affAI()
 {
     let docMain = document.querySelector('main')
@@ -22,16 +71,23 @@ function affAI()
 					<div id="scorePlayer1" class="fs-2">0</div>
 					<div id="scorePlayer2" class="fs-2">0</div>
 				</div>
-				<div class="canvas-container">
+				<div class="score-names d-flex pb-0 mb-0 justify-content-between align-items-end">
+					<div id="namePlayer1" class="d-flex pb-0 mb-0 align-items-end">You</div>
+					<div id="namePlayer2" class="d-flex pb-0 mb-0 align-items-end">Bot</div>
+				</div>	
+				<div class="canvas-container p-0">
 					<canvas id="menuCanvas" class="w-100" height="400"></canvas>
 					<div class="replayBlock">
-						<h2 id="winMessage"></h2>
-						<h2 class="fw-bold" data-translate="true">Difficulté</h2>
+						<!--h2 id="winMessage"></h2>
 						<div class="button-group">
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="authentificatio">Très Facile</button>
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="authentification">Facile</button>
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="register">Intermediaire</button>
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="OSCOUR">Pongiste Professionel</button>
+						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Replay</button>
+						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Change Difficulty</button-->
+
+						<h2 class="fw-bold" data-translate="true"> Difficulté :</h2>
+						<div class="button-group">
+						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="easyMode">Facile</button>
+						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="normalMode">Intermediaire</button>
+						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="hardMode">Pongiste Professionnel</button>
 						</div>
 					</div>
 				</div>
@@ -39,22 +95,8 @@ function affAI()
 		</div>
 	</div>
     `
-    document.getElementsByClassName("replayBlock")[0].style.display = "block"
+    document.querySelector(".replayBlock").style.display = "block"
     addScript("/js/indexPong.js", callbackAI)
     tradNewPage()
-    buttons = document.querySelectorAll('main button')
-    buttons.forEach( button => {
-	button.addEventListener("click", () => {
-	canvas = document.getElementById("menuCanvas")
-	parents = canvas.parentElement
-	canvas.remove()
-	let a = document.createElement("canvas")
-	a.id = "AICanvas"
-	a.class = "w-100"
-	a.height = 400
-	parents.append(a)
-	document.getElementsByClassName("replayBlock")[0].style.display = "none"
-	addScript("/js/pong.js", other)
-	})
-	})
+    manageButtons()
 }
