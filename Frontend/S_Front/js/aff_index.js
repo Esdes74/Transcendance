7701f8bf92b526cd3e17b5111de8b4bf6ca9ba2b
Loic Mohin <loic.mohin@gmail.com>
2025-01-05 15:34:48 +0100

    added tooltips to index, correctly cleaning them on page change

--- Frontend/S_Front/js/aff_index.js
+++ Frontend/S_Front/js/aff_index.js
@@ -11,15 +11,15 @@ function affIndex()
 		<div class="container text-center my-5">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
-				<h1 class="mb-4 fw-bold" data-translate="true">Pong Majestueux</h1>
+				<h1 class="mb-4 fw-bold"><span data-translate="true">Bienvenue</span></h1>
 				<div class="canvas-container">
 					<canvas id="indexCanvas" class="w-100" height="400"></canvas>
 					<div class="replayBlock">
-						<h2 class="fw-bold" data-translate="true">Bienvenue</h2>
+						<h2 class="fw-bold" data-translate="true">Jouer :</h2>
 						<div class="button-group">
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="authentification">Solo</button>
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="register">Duo </button>
-						<span data-bs-toggle="tooltip" data-bs-placement="bottom" title='Log-in to play tournaments'>
+						<button data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="right" title="Affronte une IA au Pong!" class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="authentification">Solo</button>
+						<button data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="right" title="1vs1 en local contre un ami!" class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="register">Duel </button>
+						<span data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="bottom" title="Log-in to play tournaments">
 							<button disabled style="pointer-events: none;" class="btn btn-outline-light fw-bold" value="register"><span data-translate="true">Tournoi</span> <i class="bi bi-exclamation-triangle m-2 fw-bold col-6"></i>
 							</button>
 						</span>
@@ -34,18 +34,12 @@ function affIndex()
 	const originalTitle = tooltipElement.getAttribute('title');
 	console.log(document.getElementById('tooltip'))
 	console.log(originalTitle)
-    var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-      tooltipTriggerList.forEach(function (tooltipTriggerEl) {
-        new bootstrap.Tooltip(tooltipTriggerEl, {
-			html: true
-		})
-	})
-	/*if (is_logged())
-	{
+        var tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+        tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl) })
 
-	}*/
-    document.getElementsByClassName("replayBlock")[0].style.display = "block"
-    addScript("/js/indexPong.js", callbackIndex)
-    tradNewPage()
-    getMainButtons()
+	document.getElementsByClassName("replayBlock")[0].style.display = "block"
+	addScript("/js/indexPong.js", callbackIndex)
+	tradNewPage()
+	getMainButtons()
 }
--- Frontend/S_Front/js/links.js
+++ Frontend/S_Front/js/links.js
@@ -9,16 +9,26 @@ function getMainButtons()
 {
     let buttons = document.querySelectorAll('main button')
     buttons.forEach( button => {
-	button.addEventListener("click", () => updatePage(button.value))
+	button.addEventListener("click", () =>
+	updatePage(button.value)
+	)
     })
+}
+
+function cleanTooltips()
+{
+    let tooltips = document.querySelectorAll('.tooltip')
+    tooltips.forEach( tooltip => {
+	tooltip.remove()})
 }
 
 function updatePage(value)
 {
-	if (value === "index")
+    if (value === "index")
 	value = ""
     if (value === history.state.pageID || value === "expand")
     	return
+    cleanTooltips()
     history.pushState({pageID: value}, '', "/" + value)
     console.log("event dispatched");
     const event = new CustomEvent('pageChanged');
