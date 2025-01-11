d3ce2fc7fb4c354e07e0d01dde649d7d0a6b394b
Loic Mohin <loic.mohin@gmail.com>
2025-01-11 23:15:39 +0100

    removing useless margin properties, resizing a bit

--- Frontend/S_Front/js/aff_ai.js
+++ Frontend/S_Front/js/aff_ai.js
@@ -98,23 +98,18 @@ function affAI()
 		<div class="container text-center my-5">
 		<div class="row justify-content-center">
 			<div class="col-md-8">
-				<h1 class="mb-4 fw-bold" data-translate="true">Mode Solo</h1>
-				<div class="score-board d-flex justify-content-around mb-3">
+				<h1 class="mb-2 fw-bold" data-translate="true">Mode Solo</h1>
+				<div class="score-board d-flex justify-content-around">
 					<div id="scorePlayer1" class="fs-2">0</div>
 					<div id="scorePlayer2" class="fs-2">0</div>
 				</div>
-				<div class="score-names d-flex pb-0 mb-0 justify-content-between align-items-end">
+				<div class="score-names d-flex justify-content-between align-items-end">
 					<div id="namePlayer1" class="d-flex pb-0 mb-0 align-items-end">You</div>
 					<div id="namePlayer2" class="d-flex pb-0 mb-0 align-items-end">Bot</div>
 				</div>	
 				<div class="canvas-container p-0">
 					<canvas id="menuCanvas" class="w-100" height="400"></canvas>
 					<div class="replayBlock">
-						<!--h2 id="winMessage"></h2>
-						<div class="button-group">
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Replay</button>
-						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true">Change Difficulty</button-->
-
 						<h2 class="fw-bold" data-translate="true"> Difficulté :</h2>
 						<div class="button-group">
 						<button class="btn btn-outline-light m-2 fw-bold" data-translate="true" value="easyMode">Facile</button>
