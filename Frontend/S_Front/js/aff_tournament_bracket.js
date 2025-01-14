8f5301c656ab191b77a216bb91422526c437424c
tdutel <tdutel@student.42lyon.fr>
2025-01-14 12:25:17 +0100

    tournament bracket front page

--- Frontend/S_Front/js/aff_tournament_bracket.js
+++ Frontend/S_Front/js/aff_tournament_bracket.js
@@ -5,26 +5,28 @@ function paringPrintLoop(pair)
 
 	// creation du texte VS
 	const current_game = document.createElement('div');
-	current_game.className = 'col';
+	current_game.className = 'col-12 fs-3';
 	current_game.textContent = pair[0] + " vs " + pair[1];
+	current_game.style.color = 'white';
 	current_game.setAttribute('data-translate', 'true');
 
 	// creation du btn VS
 	const startBtn = document.createElement('button');
-	startBtn.className = 'btn btn-outline-light m-2';
-	startBtn.textContent = 'Commencer';
+	startBtn.className = 'btn btn-outline-light m-2 text-center rounded-pill';
+	startBtn.textContent = 'Commencer !';
 	startBtn.setAttribute('data-translate', 'true');
 	tradDiv(startBtn);
 
 	const newDiv = document.createElement('div');
-	newDiv.className = 'row';
+	newDiv.className = 'p-2 rounded-pill m-5 d-inline-block bubble';
+	newDiv.id = 'newDiv'
 	newDiv.appendChild(current_game);
 	newDiv.appendChild(startBtn);
 
 	addEvent(pair[0], pair[1], startBtn);
 
 	algoDiv.appendChild(newDiv);
-	newDiv.style.border = '1px solid green';
+	newDiv.style.border = '3px solid white';
 
 }
 
@@ -82,7 +84,7 @@ async function affTournamentBracket_sendRequest(data, function_name)
 function getHTML()
 {
 	return (`
-	<h1 class="display-1" data-translate="true">Déroulement des matchs</h1>
+	<h1 class="display-1 text-center" data-translate="true">Déroulement des matchs</h1>
 
 	<div class="container text-center" id="algo" data-translate="true" >
 	</div>
