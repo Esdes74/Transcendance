9dd6b42fdd8a68a3af15588fe856f691f0b4b66e
Loic Mohin <loic.mohin@gmail.com>
2025-01-18 23:50:48 +0100

    trad in uppercase for 404 page, stop anim correcty on page change

--- Frontend/S_Front/js/404Pong.js
+++ Frontend/S_Front/js/404Pong.js
@@ -3,15 +3,22 @@ function init404Pong()
 	const gameSettings = {
 		canvas: document.getElementById('404Canvas'),
 		
-		printBall: true,	
 		textX: 0.5,				
 		textY: 0.5,
 
 		textSpeedX: 0.005,
 		textSpeedY: 0.004,
+		
+		stopAnim: false
 	};
-
-
+	window.addEventListener('popstate', (event) =>
+	{
+		gameSettings.stopAnim = true
+	})
+	document.addEventListener('pageChanged', (event) =>
+	{
+		gameSettings.stopAnim = true
+	})
 	resize404Canvas(gameSettings);
 	loop404(gameSettings);
 }
@@ -38,14 +45,11 @@ function loop404(gameSettings) {
 	gameSettings.textX += gameSettings.textSpeedX;
 	gameSettings.textY += gameSettings.textSpeedY;
 	if (gameSettings.textX <= 0)
-	{
 		gameSettings.textSpeedX *= -1
-	}
 	if (gameSettings.textX * gameSettings.canvas.width >= gameSettings.canvas.width - 55)
-	{
-	gameSettings.textSpeedX *= -1
-	}
+		gameSettings.textSpeedX *= -1
 	if (gameSettings.textY * gameSettings.canvas.height <= 20 || gameSettings.textY >= 1)
 	gameSettings.textSpeedY *= -1 
-requestAnimationFrame(() => loop404(gameSettings));
+	if (gameSettings.stopAnim === false)
+		requestAnimationFrame(() => loop404(gameSettings));
 }
