5c23de5df0cf2a56d73c3bd298fe5e5a863919b0
Loic Mohin <loic.mohin@gmail.com>
2025-01-20 03:53:47 +0100

    registertoft error management

--- Frontend/S_Front/js/registertoft.js
+++ Frontend/S_Front/js/registertoft.js
@@ -7,7 +7,6 @@ async function loadRegisterToFT()
 	};
 
 	try {
-		// Envoie les données à l'API
 		const response = await fetch('api/remote_oauth/forty_two_auth/', {
 			method: 'POST',
 			headers: {
@@ -16,23 +15,14 @@ async function loadRegisterToFT()
 			body: JSON.stringify(data),
 			credentials: 'include'
 		});
-
-		// Vérifie la réponse de l'API
 		if (response.ok) {
 			const result = await response.json();
-			console.log('Réponse de l\'API :', result.uri);
-
-			// uri = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=${state}`;
 			window.location.assign(result.uri);
 		} else {
-			// Affiche un message d'erreur si la connexion échoue
-			const error = await response.json();
-			console.error('Erreur :', error);
-			alert('Échec de la connexion : ' + JSON.stringify(error));
+			updatePage("50X")
 		}
 	} catch (error) {
-		console.error('Erreur lors de la connexion :', error);
-		alert('Une erreur est survenue. Veuillez réessayer.');
+		updatePage("50X")
 	}
 }
 
@@ -45,8 +35,5 @@ function generateRandomString(length) {
 	}
 	return result;
 }
-
-//loadRegisterToFT()
-
 // Keep this just in case
 // uri = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
