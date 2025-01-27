83e3dc9b544c595e8b2b9eca0c96c33822008de6
Loic Mohin <loic.mohin@gmail.com>
2025-01-27 16:55:02 +0100

    adding confirmation button to user delete

--- Frontend/S_Front/js/aff_settings.js
+++ Frontend/S_Front/js/aff_settings.js
@@ -49,8 +49,11 @@ async function affSettings()
 						<hr/>
 						<button class="btn btn-outline-light fw-bold fs-6 mt-2 mb-2" id="save-change" data-translate="true">Sauvegarder les Changements</button>	
 						<p class="my-0 py-1"><span class="bg-success text-white rounded fw-bold fs-6" id="validation-message" data-translate="true"></span></p>
-						<p class="mt-1 mb-2" id="logout"><span class="logout-btn fs-6 fw-bold text-white px-2 py-1 border border-2 rounded bg-danger" data-translate="true"> Se Déconnecter </span></p>
-						<p class="del-btn mb-0" id="deleteAccount"><span class="del-btn fs-6 fw-bold text-white px-2 py-1 border border-2 rounded bg-dark" data-translate="true">Supprimer son compte</span></p>
+						<p class="mt-1 mb-3" id="logout"><span class="logout-btn fs-6 fw-bold text-white px-2 py-1 border border-2 rounded bg-danger" data-translate="true"> Se Déconnecter </span></p>
+						<div class="mb-0">
+						<p class="del-btn mb-2" id="deleteAccount"><span class="fs-6 fw-bold text-white px-2 py-1 border border-2 rounded bg-dark" data-translate="true">Supprimer son compte</span></p>
+						<p style="display: none; width: 160px; margin: 0 auto;" class="del-btn mb-0 fs-6 fw-bold text-white py-1 border border-2 rounded bg-dark" id="confirmDelete"><span data-translate="true">Confirmer</span><i class="ms-2 bi bi-exclamation-triangle fw-bold"></i></p>
+						</div>
 						</div>
 					</div>
 				</div>
@@ -62,7 +65,14 @@ async function affSettings()
 	new bootstrap.Tooltip(tooltip)
 	document.querySelectorAll('.replayBlock')[0].style.display = "block"
 	document.getElementById('logout').addEventListener("click", () => logoutUser())
-	document.getElementById('deleteAccount').addEventListener("click", () => deleteUser())
+	document.getElementById('deleteAccount').addEventListener("click", () => {
+		myConfirmButton = document.getElementById('confirmDelete')
+		if (myConfirmButton.style.display != "block")
+			myConfirmButton.style.display = "block"
+		else
+			myConfirmButton.style.display = "none"
+	})
+	document.getElementById('confirmDelete').addEventListener("click", () => deleteUser())
 	addScript("/js/settings.js", callbackSettings)
 	addScript("/js/settingsPong.js", callbackSettingsPong)
 	tradNewPage()
