c95bde3427fcbf071fce1c1b06b00387ea1b1cb4
Loic Mohin <loic.mohin@gmail.com>
2025-01-27 01:14:28 +0100

    removing keys from settings.py

--- Backend/S_Pong/Pong/Pong/settings.py
+++ Backend/S_Pong/Pong/Pong/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n_!3%#y5*3ar+c8!8=nqnk@dx9@=qt%o)u%ignl4^^#46v-fy9'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
