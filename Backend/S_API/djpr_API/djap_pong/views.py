88c15cd9e38e540927340ccc26ad6eca27fb4014
Loic Mohin <loic.mohin@gmail.com>
2025-01-28 11:03:11 +0100

    del: Save

--- Backend/S_API/djpr_API/djap_pong/views.py
+++ Backend/S_API/djpr_API/djap_pong/views.py
@@ -1,6 +1,3 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# # Create your views here.
-# def launch_view(request):
-# 	print('launch_view')
-# 	return render(request, 'pong/launch.html') # renvoie la page de login
+# Create your views here.
